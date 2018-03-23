bb06e7bac3d51c20fd3e1d99e448def4f396ad0a
香饽饽 <1377699408@qq.com>
2018-03-23 11:40:35 +0800

    utils

---
+++ config.py
@@ -0,0 +1,4 @@
+USE_FACE = True
+CHROME_DRIVER_PATH = '/usr/bin/chromedriver'
+PHANTOMJS_PATH = ''
+SCREENSHOT_PATH = 'screenshot'
---
+++ utils.py
@@ -0,0 +1,101 @@
+import json
+import os
+from datetime import datetime
+
+from logzero import logger
+from selenium import webdriver
+from selenium.common.exceptions import UnexpectedAlertPresentException
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+from config import USE_FACE, CHROME_DRIVER_PATH, PHANTOMJS_PATH, SCREENSHOT_PATH
+
+
+class open_driver(object):
+    def __init__(self, width=1920, height=7000, cookie_domain=None, load_image=False):
+        self.width = width
+        self.height = height
+        self.cookie_domain = cookie_domain
+        self.load_image = load_image
+        if self.cookie_domain:
+            self.cookie_file = os.path.join(os.path.expanduser('~'), self.cookie_domain[1:] + '.json')
+            if not os.path.exists(self.cookie_file):
+                raise Exception(f'{self.cookie_file} 不存在，请设置')
+
+    def __enter__(self):
+        if USE_FACE:
+            self.driver = webdriver.Chrome(CHROME_DRIVER_PATH)
+            # self.driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver')
+
+            logger.info('chrome浏览器打开')
+            self.driver.get('http://www.baidu.com')
+        else:
+            dcap = dict(DesiredCapabilities.PHANTOMJS)
+            dcap["phantomjs.page.settings.userAgent"] = (
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"
+            )
+            service_args = []
+
+            if not self.load_image:
+                service_args.append('--load-images=no')
+            service_args.append('--disk-cache=yes')
+            service_args.append('--ignore-ssl-errors=true')
+            self.driver = webdriver.PhantomJS(PHANTOMJS_PATH, desired_capabilities=dcap, service_args=service_args)
+            logger.info('phantomjs浏览器打开')
+        self.driver.set_window_size(self.width, self.height)
+        self.add_cookie()
+        js = "window.scrollTo(0, document.body.scrollHeight)"
+        self.driver.scroll_buttom = lambda: self.driver.execute_script(js)
+        return self.driver
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_tb:
+            self.driver.get_screenshot_as_file(
+                f"{SCREENSHOT_PATH}/excep_{datetime.now().strftime('%Y-%m-%d %H %M %S')}.png")
+        self.store_cookie()
+        logger.info("浏览器关闭")
+        self.driver.close()
+        self.driver.quit()
+        if exc_tb:
+            logger.error("出现异常")
+            logger.error(exc_type)
+            logger.error(exc_val)
+            logger.error(exc_tb)
+            return False
+
+    def add_cookie(self):
+        if not self.cookie_domain:
+            return
+        logger.info('加载cookie')
+        self.driver.delete_all_cookies()
+        cookies = json.load(open(self.cookie_file, 'r'))
+        for c in cookies:
+            if c.get('domain') != self.cookie_domain:
+                continue
+            self.driver.add_cookie(c)
+        logger.info('加载完成')
+
+    def store_cookie(self):
+        if not self.cookie_domain:
+            return
+        logger.info('存储cookie')
+        new_cookies = self.driver.get_cookies()
+        json.dump(new_cookies, open(self.cookie_file, 'w'))
+        logger.info('加载完成')
+
+
+class track_alert(object):
+    def __init__(self, driver):
+        self.driver = driver
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type == UnexpectedAlertPresentException:
+            if exc_tb:
+                self.driver.get_screenshot_as_file(
+                    f"{SCREENSHOT_PATH}/alert_{datetime.now().strftime('%Y-%m-%d %H %M %S')}.png")
+            import re
+            msg = re.findall(r'{Alert text : (.*)}', exc_val.msg)[0]
+            logger.error(msg)
+            return True
