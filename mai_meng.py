86b570bfe6d90c7e1c18fb9a66af900b1625d1e1
香饽饽 <1377699408@qq.com>
2018-04-02 11:05:13 +0800

    maimeng

--- mai_meng.py
+++ mai_meng.py
@@ -9,6 +9,7 @@ COOKIE_DOMAIN = 'author.maimengjun.com'
 COOKIE_FILE = f'cookies/{COOKIE_DOMAIN[1:]}_{data["maimeng_username"]}.cookie.json'
 MANAGE_URL = 'http://author.maimengjun.com/submission'
 AUTH_OK_URL = 'http://author.maimengjun.com/submission'
+CREATE_CHAPTER_URL = 'http://author.maimengjun.com/submission/create_chapter'
 
 
 class MaiMeng:
@@ -29,8 +30,23 @@ class MaiMeng:
                         return
                 store_cookie(driver, COOKIE_FILE)
                 logger.info('登录成功')
-                pass
 
+                # 根据作品名称点击对应的新建章节
+                items = driver.find_elements_by_css_selector('#submission > div.container > ul > li')
+                for item in items:
+                    work_name = item.find_element_by_css_selector('div.content > h3').text
+                    logger.debug(f'发现章节{work_name}')
+                    if work_name.strip() == data['maimeng_series'].strip():
+                        new_chapter = item.find_element_by_css_selector('nav > a:nth-child(2)')
+                        new_chapter.click()
+                        time.sleep(5)
+                handles = driver.window_handles
+                if len(handles) == 1:
+                    logger.error(f'点击创建章节失败 maimeng_series={data["maimeng_series"]}')
+                    return
+                else:
+                    driver.switch_to_window(handles[1])
+                self.publish()
 
     def login(self):
         login_url = get_current_url()
@@ -42,6 +58,29 @@ class MaiMeng:
         if get_current_url() != AUTH_OK_URL:
             input('请处理登录异常，之后按回车键')
         return get_current_url() != login_url
+
+    def publish(self):
+        # 章节名称
+        self.driver.find_element_by_css_selector(
+            '#create_chapter > div.container > div.inner-container > div:nth-child(1) > div > div.field-input > input[type="text"]').send_keys(
+            data['maimeng_title'])
+        # 章节介绍
+        self.driver.find_element_by_css_selector(
+            '#create_chapter > div.container > div.inner-container > div:nth-child(2) > div > div.field-input > textarea').send_keys(
+            data['maimeng_desp'])
+        # 展示时间
+        if data['maimeng_publish_time']:
+            self.driver.find_element_by_css_selector(
+                '#create_chapter > div.container > div.inner-container > div:nth-child(3) > div > div.field-input > div > input').send_keys(
+                data['maimeng_publish_time'])
+        # 备注
+        self.driver.find_element_by_css_selector(
+            '#create_chapter > div.container > div.inner-container > div:nth-child(4) > div > div.field-input > textarea').send_keys(
+            data['maimeng_comment'])
+
+        # 漫画原稿
+        self.driver.find_element_by_css_selector(
+            '#create_chapter > div.container > div.inner-container > div:nth-child(5) > div > div.field-input > ul > li > img')
 
 
 if __name__ == '__main__':
