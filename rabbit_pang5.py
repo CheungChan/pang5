4afd0ec34bab3d9a7eed7cd757804de812b02966
hgz <308602626@qq.com>
2018-05-03 15:06:40 +0800

    update mysql filed

--- rabbit_pang5.py
+++ rabbit_pang5.py
@@ -44,18 +44,26 @@ def callback(ch, method, properties, body):
 
     mysql_id = rabbitInfo['mysql_id']
     row = db.query('SELECT * FROM  chapter_chapter where id= :id_num', id_num=mysql_id)
-    logger.error(len(row))
-    if  not len(row):
+    if not len(row.all()):
         return
     # 下载封面
+    print(row.all())
     if row[0]['cover_img']:
-        content = requests.get('http://pang5web.oss-cn-beijing.aliyuncs.com/' + row[0]['cover_img']).content
+        if row[0]['cover_img'][0:4]=='http':
+            content = requests.get(row[0]['cover_img']).content
+
+        else:
+            content = requests.get('http://pang5web.oss-cn-beijing.aliyuncs.com/' + row[0]['cover_img']).content
         file = BytesIO()
         file.write(content)
-        Image.open(file).convert("RGB").save('./images/封面.jpg')
+        Image.open(file).convert("RGB").save('./images/封面.png')
     i = 1
     for img in json.loads(row[0]['chapter_imgs']):
-        content = requests.get('http://pang5web.oss-cn-beijing.aliyuncs.com/' + img).content
+        if img[0:4]=="http":
+            content = requests.get(img).content
+
+        else:
+            content = requests.get('http://pang5web.oss-cn-beijing.aliyuncs.com/' + img).content
         file = BytesIO()
         file.write(content)
         Image.open(file).convert("RGB").save(os.path.join(pwd, "images", "章节", str(i) + ".jpg"))
@@ -66,20 +74,24 @@ def callback(ch, method, properties, body):
     import tencent
     import mai_meng
     # 平台
-    userinfo = db.query('SELECT * FROM subscriber_platformsubscriber where id=:platform_subsriber_id_id',
-                        platform_subsriber_id_id=row[0]['platform_subsriber_id_id'])
+    works_info = db.query('SELECT * FROM work_works where id=:work_id',work_id=row[0]['works_id_id'])
+    print(works_info.all())
+    if not len(works_info.all()):
+        return
+    userinfo=db.query('SELECT * FROM  subscriber_platformsubscriber where id =:id',id=works_info[0]['platform_subsriber_id_id'])
+    print(userinfo.all())
     if userinfo[0]['platform'] == 'qingdian':
         data['qingdian_username'] = userinfo[0]['platform_username']
         data['qingdian_password'] = userinfo[0]['platform_password']
-        data['qingdian_series'] = row[0]['works_name']
-        data['qingdian_title'] = row[0]['chapter_name']
+        data['qingdian_series'] = works_info[0]['name']
+        data['qingdian_title'] = row[0]['name']
 
         qingdian.main(mysql_id)
     elif userinfo[0]['platform'] == 'qq':
         data['qq_username'] = userinfo[0]['platform_username']
         data['qq_password'] = userinfo[0]['platform_password']
-        data['qq_comic_id'] = row[0]['works_id']
-        data['qq_chapter_title'] = row[0]['chapter_name']
+        data['qq_comic_id'] = works_info[0]['third_id']
+        data['qq_chapter_title'] = row[0]['name']
         data['qq_use-appoint'] = row[0]['is_publish_clock']
         data['qq_chapter-publish-time'] = row[0]['publish_clock_time']
         tencent.main(mysql_id)
@@ -87,8 +99,8 @@ def callback(ch, method, properties, body):
         data['net_username'] = userinfo[0]['platform_username']
         data['net_password'] = userinfo[0]['platform_password']
         data['net-use-appoint'] = row[0]['is_publish_clock']
-        data['net_series_title'] = row[0]['works_name']
-        data['net_title_text'] = row[0]['chapter_name']
+        data['net_series_title'] = works_info[0]['name']
+        data['net_title_text'] = row[0]['name']
         data['net-login'] = userinfo[0]['platform_login_type']
 
         if row[0]['is_publish_clock']:
@@ -108,16 +120,16 @@ def callback(ch, method, properties, body):
     elif userinfo[0]['platform'] == 'maimeng':
         data['maimeng_username'] = userinfo[0]['platform_username']
         data['maimeng_password'] = userinfo[0]['platform_password']
-        data['maimeng_series'] = row[0]['works_name']
-        data['maimeng_title'] = row[0]['chapter_name']
+        data['maimeng_series'] = works_info[0]['name']
+        data['maimeng_title'] = row[0]['name']
         data['maimeng_publish_time'] = row[0]['publish_clock_time']
         mai_meng.main(mysql_id)
     elif userinfo[0]['platform'] == 'u17':
         data['u17_username'] = userinfo[0]['platform_username']
         data['u17_password'] = userinfo[0]['platform_password']
-        data['u17_comic_id'] = row[0]['works_id']
-        data['u17_chapter'] = row[0]['chapter_name']
-        data['u17_series'] = row[0]['works_name']
+        data['u17_comic_id'] =works_info[0]['third_id']
+        data['u17_chapter'] = row[0]['name']
+        data['u17_series'] = works_info[0]['name']
     else:
         logger.error('未知平台')
 
@@ -157,5 +169,5 @@ def insert_rabbit(format):
 if __name__ == '__main__':
     # row = db.query('SELECT * FROM  chapter_chapter where id= :id_num', id_num=17)
     # print(row[1])
+    insert_rabbit({'mysql_id':5})
     main()
-    # insert_rabbit({'mysql_id':5})
